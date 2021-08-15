bc7c24cd423d9f432184819e9ea914f8d51ad371
sliu001 <sliu001@users.noreply.github.com>
2021-08-15 17:16:17 -0400

    Update NumberOfIslands_LC200.cs

--- NumberOfIslands_LC200.cs
+++ NumberOfIslands_LC200.cs
@@ -7,6 +7,11 @@ Input: grid = [
   ["0","0","0","1","1"]
 ]
 Output: 3
+Method1: DFS recursive + mark 1 to 0 if visited.
+Method2: DFS recursive + "boolean visited[][] = new boolean[n][m];" to record if dot has been visited.
+Method3: DFS iterative -> using Stack<Tuple<int,int>> or Queue to push surrounding 4 points, + mark 1 to 0 if visited
+Method4: Union-Find. Need to create a seperate "class UF with Union()/Find()/.Count", and convert 2 dimensional indecies to 1 dimension index, and union.
+https://leetcode.com/problems/number-of-islands/discuss/56364/Java-Union-Find-Solution
 */
 
 public class Solution {
