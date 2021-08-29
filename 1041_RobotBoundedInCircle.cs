31e099cadaadcb41276c54866852b2031dea0198
sliu001 <sliu001@users.noreply.github.com>
2021-08-28 21:53:40 -0400

    Update and rename RobotBoundedInCircle.cs to 1041_RobotBoundedInCircle.cs

--- 1041_RobotBoundedInCircle.cs
+++ 1041_RobotBoundedInCircle.cs
@@ -6,6 +6,15 @@ LC1041: On an infinite plane, a robot initially stands at (0, 0) and faces north
 The robot performs the instructions given in order, and repeats them forever.
 
 Return true if and only if there exists a circle in the plane such that the robot never leaves the circle.
+
+Input: instructions = "GGLLGG"
+Output: true
+Explanation: The robot moves from (0,0) to (0,2), turns 180 degrees, and then returns to (0,0).
+When repeating these instructions, the robot remains in the circle of radius 2 centered at the origin.
+
+Input: instructions = "GG"
+Output: false
+Explanation: The robot moves north indefinitely.
 */
 
 public class Solution {
